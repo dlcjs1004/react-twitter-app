7302e953d0d9115df38b70949910c8e646480852
dlcjs1004 <dlcjs1004@naver.com>
2025-01-11 13:50:05 +0900

    feat: Firebase Auth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Router from 'components/Router';
 import { Layout } from 'components/Rayout';
+import { getAuth } from "firebase/auth";
+import { app } from 'firebaseApp';
+import { useState } from 'react';
 
 function App() {
+  const auth = getAuth(app);
+  //currentUser가 있으면 true, 없으면 false 반환하도록
+  const [isAuthenticated, SetIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+  console.log(isAuthenticated);
+
   return (
     <Layout>
       <Router />
