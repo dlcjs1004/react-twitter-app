45d800224a974ca1112d8035090c59ea10214ff1
dlcjs1004 <dlcjs1004@naver.com>
2025-01-26 16:52:07 +0900

    feat: 해시태그 폼 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import PostForm from "components/posts/PostForm";
 import PostBox from "components/posts/PostBox";
-import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import AuthContext from "context/AuthContext";
 import { db } from "firebaseApp";
 export interface PostProps {
@@ -14,6 +14,7 @@ export interface PostProps {
   liekes?:string[];
   likeCount?: number;
   comments?: any;
+  hashTags?: string[];
 }
 
 export default function HomePage() {
